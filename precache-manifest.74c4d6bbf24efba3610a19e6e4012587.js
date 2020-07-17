59c6a7df964193241288a6beeb77ad9c2ed6b5b6
AustinWRye <RyeAustinW@gmail.com>
2020-07-16 23:05:15 -0400

    Add website build

---
+++ precache-manifest.74c4d6bbf24efba3610a19e6e4012587.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2705c74d60048e7c4a81eaab6cab2e1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89f2ac387a812b6ffbf6",
+    "url": "/static/css/main.667682b3.chunk.css"
+  },
+  {
+    "revision": "57c5175d66041e990d96",
+    "url": "/static/js/2.063a4dd5.chunk.js"
+  },
+  {
+    "revision": "f489ef63a56c676a2aeebf9bd1f90580",
+    "url": "/static/js/2.063a4dd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f2ac387a812b6ffbf6",
+    "url": "/static/js/main.fa0dd618.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "56ba0ad3e453b1ae5a8c1d4ee41b4a41",
+    "url": "/static/media/resume.56ba0ad3.pdf"
+  }
+]);
